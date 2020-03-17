f779f68c1139ddacf3185e4c20ca629ad49879b9
ronie <ronie@DESKTOP-085MPR7>
2020-03-16 22:13:19 -0300

    finalizado execicio 13

--- Curso-java-basico/src/com/loiane/cursojava/exercicios_aula13/Exercicio_13.java
+++ Curso-java-basico/src/com/loiane/cursojava/exercicios_aula13/Exercicio_13.java
@@ -1,10 +1,31 @@
 package com.loiane.cursojava.exercicios_aula13;
 
+import java.util.Scanner;
+
 public class Exercicio_13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite quanto você ganha por hora");
+		double ganhaPorHora = scan.nextDouble();
+		
+		System.out.println("Digite quantas horas você trabalho no mês");
+		double horasTrabalhada = scan.nextDouble();
+		
+		double salario = ganhaPorHora*horasTrabalhada;
+		
+		double inss = salario*0.08;
+		double impostoDeRenda = salario*0.11;
+		double sindicato = salario*0.05;
+		double salarioLiquido = salario-inss-impostoDeRenda-sindicato;
+		
+		
+		System.out.println("Você pagou ao INSS :"+inss);
+		System.out.println("Você pagou ao Sindicato :"+sindicato);
+		System.out.println("Seu com os desconto é :"+salarioLiquido	);
+	   
+	   
 	}
 
 }
